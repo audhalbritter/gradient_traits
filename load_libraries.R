65d5b3dbd1a3ff165d5d195b80be1b76c2f03148
audhalbritter <me@audhalbritter.com>
2023-10-20 10:59:46 +0200

    file for loading library

---
+++ load_libraries.R
@@ -0,0 +1,12 @@
+# libraries
+
+library(targets)
+library(tidyverse)
+library(janitor)
+library(vegan)
+library(ggvegan)
+library(traitstrap)
+library(dataDocumentation)
+library(dataDownloader)
+library(broom)
+library(glue)
