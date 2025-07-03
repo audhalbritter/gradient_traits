9642d2dfef775efd63e1073971c3dd9b44f8e95e
audhalbritter <me@audhalbritter.com>
2025-07-03 20:44:29 +0200

    Refactor download_plan.R and import_plan.R for improved readability and organization of data download commands

--- R/download_plan.R
+++ R/download_plan.R
@@ -6,140 +6,147 @@ download_plan <- list(
   # trait data
   tar_target(
     name = download_traits_sv,
-    command = get_file(node = "smbqh",
-                       file = "PFTC4_Svalbard_2018_Gradient_Traits.csv",
-                       path = "data/",
-                       remote_path = "Traits"),
-    format = "file"
+    command = get_file(
+      node = "smbqh",
+      file = "PFTC4_Svalbard_2018_Gradient_Traits.csv",
+      path = "data/",
+      remote_path = "Traits"
     ),
+    format = "file"
+  ),
 
   # community data
   tar_target(
     name = download_community_sv,
-    command = get_file(node = "smbqh",
-                       file = "PFTC4_Svalbard_2018_Community_Gradient.csv",
-                       path = "data",
-                       remote_path = "Community"),
+    command = get_file(
+      node = "smbqh",
+      file = "PFTC4_Svalbard_2018_Community_Gradient.csv",
+      path = "data",
+      remote_path = "Community"
+    ),
     format = "file"
   ),
-  
+
   # Peru Data
   # trait data
   tar_target(
     name = download_traits_pe,
-    command = get_file(node = "gs8u6",
-                       file = "PFTC3-Puna-PFTC5_Peru_2018-2020_FunctionalTraits_clean.csv",
-                       path = "data/",
-                       remote_path = "traits"),
+    command = get_file(
+      node = "gs8u6",
+      file = "PFTC3-Puna-PFTC5_Peru_2018-2020_FunctionalTraits_clean.csv",
+      path = "data/",
+      remote_path = "traits"
+    ),
     format = "file"
   ),
-  
+
   # community data
   tar_target(
     name = download_community_pe,
-    command = get_file(node = "gs8u6",
-                       file = "PFTC3-Puna-PFTC5_Peru_2018-2020_CommunityCover_clean.csv",
-                       path = "data",
-                       remote_path = "community"),
+    command = get_file(
+      node = "gs8u6",
+      file = "PFTC3-Puna-PFTC5_Peru_2018-2020_CommunityCover_clean.csv",
+      path = "data",
+      remote_path = "community"
+    ),
     format = "file"
   ),
-  
+
   # China Data
   # trait data
   tar_target(
     name = download_traits_leaf_ch,
-    command = get_file(node = "f3knq",
-                       file = "PFTC1.2_China_2015_2016_LeafTraits.csv",
-                       path = "data/",
-                       remote_path = "Traits"),
+    command = get_file(
+      node = "f3knq",
+      file = "PFTC1.2_China_2015_2016_LeafTraits.csv",
+      path = "data/",
+      remote_path = "Traits"
+    ),
     format = "file"
   ),
-  
   tar_target(
     name = download_traits_chem_ch,
-    command = get_file(node = "f3knq",
-                       file = "PFTC1.2_China_2015_2016_ChemicalTraits.csv",
-                       path = "data/",
-                       remote_path = "Traits"),
+    command = get_file(
+      node = "f3knq",
+      file = "PFTC1.2_China_2015_2016_ChemicalTraits.csv",
+      path = "data/",
+      remote_path = "Traits"
+    ),
     format = "file"
   ),
-  
+
   # community data
   tar_target(
     name = download_community_ch,
-    command = get_file(node = "f3knq",
-                       file = "transplant.sqlite",
-                       path = "data",
-                       remote_path = "Community"),
+    command = get_file(
+      node = "f3knq",
+      file = "transplant.sqlite",
+      path = "data",
+      remote_path = "Community"
+    ),
     format = "file"
   ),
-  
+
   # meta data
   tar_target(
     name = download_meta_ch,
     command = "data/metaCH.csv",
     format = "file"
   ),
-  
-  
+
   # Norway Data
   # vegetation
   tar_target(
     name = download_community_no,
-    command =  get_file(node = "pk4bg",
-                        file = "7_Three-D_clean_cover_2019-2022.csv",
-                        path = "data"),
+    command = get_file(
+      node = "pk4bg",
+      file = "7_Three-D_clean_cover_2019-2022.csv",
+      path = "data"
+    ),
     format = "file"
   ),
-  
+
   # Seedclim comm data for Høgsete and Vikesland
   # file needs to be unziped
   tar_target(
     name = download_community_no2,
-    command =  get_file(node = "npfa9",
-                        file = "seedclim.2020.4.15.zip",
-                        path = "data",
-                        remote_path = "3_Community_data"),
+    command = get_file(
+      node = "npfa9",
+      file = "seedclim.2020.4.15.zip",
+      path = "data",
+      remote_path = "3_Community_data"
+    ),
     format = "file"
   ),
-  
   tar_target(
     name = download_sp_no,
-    command =  get_file(node = "pk4bg",
-                        file = "7_Three-D_clean_taxonomy.csv",
-                        path = "data"),
+    command = get_file(
+      node = "pk4bg",
+      file = "7_Three-D_clean_taxonomy.csv",
+      path = "data"
+    ),
     format = "file"
   ),
-  
+
   # traits
   tar_target(
     name = download_traits_no,
-    command =  get_file(node = "fcbw4",
-                        file = "PFTC6_clean_ElevationGradient_GlobalChangeExperiment_morphological_traits_2022.csv",
-                        path = "data",
-                        remote_path = "i. trait_data"),
+    command = get_file(
+      node = "fcbw4",
+      file = "PFTC6_clean_ElevationGradient_GlobalChangeExperiment_morphological_traits_2022.csv", # nolint: line_length_linter.
+      path = "data",
+      remote_path = "i. trait_data"
+    ),
     format = "file"
   ),
-  
+
   # meta data
   tar_target(
     name = metaTurfID_no,
     command = create_threed_meta_data()
   ),
-  
-  # South Africa Data
-  # trait data
-  tar_target(
-    name = download_traits_sa,
-    command = get_file(
-      node = "hk2cy",
-      file = "iv_PFTC7_clean_elevationgradient_traits_2023.csv",
-      path = "data/",
-      remote_path = "iv_aboveground_traits"
-    ),
-    format = "file"
-  ),
 
+  # South Africa Data
   # community data
   tar_target(
     name = download_community_sa,
@@ -150,6 +157,17 @@ download_plan <- list(
       remote_path = "i_plant_community_composition"
     ),
     format = "file"
+  ),
+
+  # trait data
+  tar_target(
+    name = download_traits_sa,
+    command = get_file(
+      node = "hk2cy",
+      file = "iv_PFTC7_clean_elevationgradient_traits_2023.csv",
+      path = "data/",
+      remote_path = "iv_aboveground_traits"
+    ),
+    format = "file"
   )
-  
 )
--- R/import_plan.R
+++ R/import_plan.R
@@ -1,38 +1,37 @@
 import_plan <- list(
-  
+
   # Svalbard Data
   # trait data
   tar_target(
     name = raw_traits_sv,
     command = read_csv(download_traits_sv)
   ),
-  
+
   # community data
   tar_target(
     name = raw_community_sv,
     command = read_csv(download_community_sv)
   ),
-  
+
   # Peru Data
   # trait data
   tar_target(
     name = raw_traits_pe,
     command = read_csv(download_traits_pe)
   ),
-  
+
   # community data
   tar_target(
     name = raw_community_pe,
     command = read_csv(download_community_pe)
   ),
-  
+
   # China Data
-  #trait data
+  # trait data
   tar_target(
     name = raw_traits_leaf_ch,
     command = read_csv(download_traits_leaf_ch)
   ),
-  
   tar_target(
     name = raw_traits_chem_ch,
     command = read_csv(download_traits_chem_ch)
@@ -40,105 +39,105 @@ import_plan <- list(
 
   # community data
   # imported in cleaning code
-  
+
   # meta
   tar_target(
     name = raw_meta_ch,
-    command = read_delim(download_meta_ch) |> 
-      clean_names() |> 
+    command = read_delim(download_meta_ch) |>
+      clean_names() |>
       select(-country, -gradient)
   ),
-  
+
   # Norway Data
   # community
   tar_target(
     name = raw_community_no,
     command = read_csv(download_community_no)
   ),
-  
+
   # sp list
   tar_target(
     name = sp_list_no,
     command = read_csv(download_sp_no)
   ),
-  
+
   # traits
   tar_target(
     name = raw_traits_no,
     command = read_csv(download_traits_no)
   ),
-  
+
   # Colorado Data
   # meta
   # tar_target(
   #   name = raw_meta_co,
   #   command = load("data/metaCommunityCO_2016.Rdata")
   # ),
-  
+
   # community
   tar_target(
     name = raw_community_co,
     command = read_csv("data/Abundance_Data_2016_final.csv")
   ),
-  
+
   # sp
   tar_target(
     name = raw_sp_co,
     command = read_csv("data/Lorah_scrubbed_notes.csv")
   ),
-  
+
   # trait
   tar_target(
     name = raw_trait_co,
     command = read_csv("data/rmbl_trait_data_master.csv")
   ),
-  
+
   # trait
   tar_target(
     name = coords_co,
     command = {
-      
-      plot_id = 1:5
-      meta <- tibble(site = c("CBT", "Road", "Pfeiler", "Cinnamon", "Almont"),
-             elevation_m = c(2699, 2810, 3171, 3458, 2475),
-             latitude_n = c(38.8817327, 38.89680392, 38.96044646, 38.99159151, 38.655455),
-             longitude_e = c(-106.9799521, -106.978584, -107.0323667, -107.0663821, -106.861632))
-      crossing(meta, plot_id) |> 
-        mutate(country = "co",
-               gradient = "C",
-               site = paste0(country, "_", site),
-               plot_id = paste0(site, "_", plot_id))
+      plot_id <- 1:5
+      meta <- tibble(
+        site = c("CBT", "Road", "Pfeiler", "Cinnamon", "Almont"),
+        elevation_m = c(2699, 2810, 3171, 3458, 2475),
+        latitude_n = c(38.8817327, 38.89680392, 38.96044646, 38.99159151, 38.655455),
+        longitude_e = c(-106.9799521, -106.978584, -107.0323667, -107.0663821, -106.861632)
+      )
+      crossing(meta, plot_id) |>
+        mutate(
+          country = "co",
+          gradient = "C",
+          site = paste0(country, "_", site),
+          plot_id = paste0(site, "_", plot_id)
+        )
     }
-      
-      
   ),
 
-  	
+
   # climate
   tar_target(
     name = bioclim,
-    command = read_csv("data/bioclim_new.csv") |> 
-      rename("annual_temperature" = bio_1,
-             "diurnal_range" = bio_2,
-             "isothermality" = bio_3,
-             "temperature_seasonality" = bio_4,
-             "max_temperture_warmest_month" = bio_5,
-             "min_temperture_coldest_month" = bio_6,
-             "temperature_annual_range" = bio_7,
-             "mean_temperture_wettest_quarter" = bio_8,
-             "mean_temperture_driest_quarter" = bio_9,
-             "mean_temperture_warmest_quarter" = bio_10,
-             "mean_temperture_coldest_quarter" = bio_11,
-             "annual_precipitation" = bio_12,
-             "precipitation_wettest_month" = bio_13,
-             "precipitation_driest_month" = bio_14,
-             "precipitation_seasonality" = bio_15,
-             "precipitation_wettest_quarter" = bio_16,
-             "precipitation_driest_quarter" = bio_17,
-             "precipitation_warmest_quarter" = bio_18,
-             "precipitation_coldest_quarter" = bio_19)
+    command = read_csv("data/bioclim_new.csv") |>
+      rename(
+        "annual_temperature" = bio_1,
+        "diurnal_range" = bio_2,
+        "isothermality" = bio_3,
+        "temperature_seasonality" = bio_4,
+        "max_temperture_warmest_month" = bio_5,
+        "min_temperture_coldest_month" = bio_6,
+        "temperature_annual_range" = bio_7,
+        "mean_temperture_wettest_quarter" = bio_8,
+        "mean_temperture_driest_quarter" = bio_9,
+        "mean_temperture_warmest_quarter" = bio_10,
+        "mean_temperture_coldest_quarter" = bio_11,
+        "annual_precipitation" = bio_12,
+        "precipitation_wettest_month" = bio_13,
+        "precipitation_driest_month" = bio_14,
+        "precipitation_seasonality" = bio_15,
+        "precipitation_wettest_quarter" = bio_16,
+        "precipitation_driest_quarter" = bio_17,
+        "precipitation_warmest_quarter" = bio_18,
+        "precipitation_coldest_quarter" = bio_19
+      )
   )
-  
-      
 )
-
