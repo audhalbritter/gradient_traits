da7ddf2093f955cecd5c9ce3a360c5d0869ebcce
audhalbritter <me@audhalbritter.com>
2025-09-03 17:07:11 +0200

    Refactor analysis and figure plans to streamline model targets and improve clarity. Updated target names for diversity models and predictions, added PCA targets for traits and bioclim data, and removed deprecated plotting commands. Enhanced data processing for model results and predictions, ensuring consistency across analyses.

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -2,88 +2,6 @@
 
 analysis_plan <- list(
 
-  # diversity across latitude
-  tar_target(
-    name = diversity_lat_model,
-    command = {
-      safelm <- safely(.f = lm)
-
-      diversity |>
-        group_by(diversity_index) |>
-        nest() |>
-        mutate(
-          model = map(.x = data, .f = ~ safelm(value ~ temperature_annual_range * country, data = .)$result),
-          result = map(model, tidy),
-          anova = map(model, car::Anova),
-          anova_tidy = map(anova, tidy),
-          prediction = map2(.x = model, .y = data, .f = predict, interval = "confidence"),
-          output = map2(.x = data, .y = prediction, ~ bind_cols(.x, .y))
-        )
-
-    }
-  ),
-
-
-  # diversity within country model
-  tar_target(
-    name = diversity_model,
-    command = {
-
-      safelm = safely(.f = lm)
-
-      diversity |>
-        group_by(country, ecosystem, diversity_index) |> 
-        nest() |>
-        mutate(model = map(.x = data, .f= ~ safelm(value ~ annual_temperature, data = .)$result),
-               result = map(model, tidy),
-               prediction = map2(.x = model, .y = data, .f = predict, interval = "confidence"),
-               output = map2(.x = data, .y = prediction, ~ bind_cols(.x, .y)))
-    }
-  ),
-
-  # testing across latitude
-  tar_target(
-    name = latitude_model,
-    command = {
-      safelm <- safely(.f = lm)
-
-      trait_mean |>
-        group_by(trait_trans, trait_fancy) |>
-        nest() |>
-        mutate(
-          model = map(.x = data, .f = ~ safelm(mean ~ annual_temperature, data = .)$result),
-          result = map(model, tidy),
-          # make new data for prediction
-          # newdata = map(.x = data, .f = ~. |>
-          #                        mutate(min)
-          prediction = map2(.x = model, .y = data, .f = predict, interval = "confidence"),
-          output = map2(.x = data, .y = prediction, ~ bind_cols(.x, .y))
-        )
-    }
-  ),
-
-  # testing within country
-  tar_target(
-    name = trait_mean_model,
-    command = {
-      safelm <- safely(.f = lm)
-
-      trait_mean |>
-        group_by(country, ecosystem, gradient, trait_trans, trait_fancy) |>
-        nest() |>
-        mutate(
-          model = map(.x = data, .f = ~ safelm(mean ~ annual_temperature, data = .)$result),
-          result = map(model, tidy),
-          # make new data for prediction
-          # newdata = map(.x = data, .f = ~. |>
-          #                        mutate(min)
-          prediction = map2(.x = model, .y = data, .f = predict, interval = "confidence"),
-          output = map2(.x = data, .y = prediction, ~ bind_cols(.x, .y))
-        )
-    }
-  ),
-
-
   # ordination reduced, all traits fewer countries
   tar_target(
     name = trait_pca,
@@ -104,14 +22,85 @@ analysis_plan <- list(
   tar_target(
     name = bioclim_pca,
     command = make_bioclim_pca(bioclim |>
-    select(country:diurnal_range, max_temperture_warmest_month, min_temperture_coldest_month, 
-    annual_precipitation, precipitation_driest_month, precipitation_driest_quarter))
+    select(country:ID, annual_temperature,
+    annual_precipitation, precipitation_seasonality, precipitation_driest_quarter, precipitation_warmest_quarter, 
+    diurnal_range, temperature_seasonality,
+    max_temperture_warmest_month, temperature_annual_range, mean_temperture_driest_quarter))
   ),
 
-  # bioclim correlation plot
+# Run models
+  # diversity models
   tar_target(
-    name = bioclim_correlation_plot,
-    command = make_bioclim_correlation_plot(bioclim)
+    name = diversity_models,
+    command = {
+      safelmer <- safely(.f = lme4::lmer)
+
+      diversity |>
+        filter(diversity_index != "sum_abundance") |>
+        group_by(diversity_index) |>
+        nest() |>
+        mutate(
+          model_null = map(.x = data, .f = ~ safelmer(value ~ 1 + (1|country), data = .)$result),
+          model_lat = map(.x = data, .f = ~ safelmer(value ~ latitude_n + (1|country), data = .)$result),
+          model_anntemp = map(.x = data, .f = ~ safelmer(value ~ annual_temperature + (1|country), data = .)$result),
+          model_temprange = map(.x = data, .f = ~ safelmer(value ~ temperature_annual_range + (1|country), data = .)$result),
+          glance_null = map(model_null, broom.mixed::glance),
+          glance_lat = map(model_lat, broom.mixed::glance),
+          glance_anntemp = map(model_anntemp, broom.mixed::glance),
+          glance_temprange = map(model_temprange, broom.mixed::glance)
+        ) |>
+        # make long table
+        pivot_longer(cols = -c(diversity_index, data),
+                     names_sep = "_",
+                     names_to = c(".value", "bioclim"))
+    }
+  ),
+
+    # Tidy results from existing models (no refit)
+  tar_target(
+    name = diversity_results,
+    command = {
+      diversity_models |>
+        filter(bioclim %in% c("lat", "anntemp", "temprange")) |>
+        mutate(
+          result = map(model, broom.mixed::tidy)
+        ) |>
+        select(diversity_index, bioclim, result) |>
+        unnest(result)
+    }
+  ),
+
+  # diversity predictions
+  tar_target(
+    name = diversity_predictions,
+    command = {
+      diversity_models |>
+        filter(bioclim != "null") |>  # Remove null models - only needed for comparison
+        mutate(
+          prediction = map2(.x = model, .y = data, .f = ~ broom.mixed::augment(.x, newdata = .y))
+        )
+    }
+  ),
+
+  # traits models
+  tar_target(
+    name = latitude_model,
+    command = {
+      safelm <- safely(.f = lm)
+
+      trait_mean |>
+        group_by(trait_trans, trait_fancy) |>
+        nest() |>
+        mutate(
+          model = map(.x = data, .f = ~ safelm(mean ~ annual_temperature, data = .)$result),
+          result = map(model, tidy),
+          # make new data for prediction
+          # newdata = map(.x = data, .f = ~. |>
+          #                        mutate(min)
+          prediction = map2(.x = model, .y = data, .f = predict, interval = "confidence"),
+          output = map2(.x = data, .y = prediction, ~ bind_cols(.x, .y))
+        )
+    }
   )
 
 )
