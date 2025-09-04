828bbe1e881c28386b4731e6c3b7cab181145f14
audhalbritter <me@audhalbritter.com>
2025-09-04 09:09:28 +0200

    Update package dependencies in _targets.R and load_libraries.R to include 'MetBrewer' for enhanced visualization capabilities. Clean up library loading for improved readability and organization.

--- load_libraries.R
+++ load_libraries.R
@@ -1,15 +1,13 @@
-# libraries
-
-library(targets)
+# Load libraries
 library(tidyverse)
-library(janitor)
 library(vegan)
 library(ggvegan)
 library(traitstrap)
 library(dataDocumentation)
-library(dataDownloader)
+library(readxl)
 library(broom)
 library(broom.mixed)
 library(glue)
 library(geodata)
 library(terra)
+library(MetBrewer)
